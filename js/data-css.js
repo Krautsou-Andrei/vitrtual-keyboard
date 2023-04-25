5a22b2ceb57fe981f08cec85179f97ed06e50494
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-04-25 08:43:35 +0300

    feat: add css dictionary

---
+++ js/data-css.js
@@ -0,0 +1,17 @@
+const CssClasses = {
+  TITLE: 'title',
+  TEXT_AREA: 'textarea',
+  KEYBOARD: 'keyboard',
+  ROW: 'row',
+  KEY: 'key',
+  KEY_ACTIVE: 'key-active',
+  TEXT: 'text',
+  TEXT_SHIFT: 'text-shift',
+  TEXT_CAPS: 'text-caps',
+  TEXT_SHIFT_CAPS: 'text-shift-caps',
+  VISUALLY_HIDDEN: 'visually-hidden',
+  RU: 'ru',
+  EN: 'en',
+};
+
+export default CssClasses;
